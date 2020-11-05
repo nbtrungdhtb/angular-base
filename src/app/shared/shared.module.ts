723ad0e4a5fe94904281891421b0cbda19d83caf
trungnguyen <nguyenbaotrung@momtech.vn>
2020-11-05 10:51:19 +0700

    add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BreadcrumbComponent} from './breadcrumb/breadcrumb.component';
 import {RouterModule} from '@angular/router';
-import { NotifySweet2Component } from './notify-sweet2/notify-sweet2.component';
 
 
 @NgModule({
     declarations: [
-        BreadcrumbComponent,
-        NotifySweet2Component
+        BreadcrumbComponent
     ],
     exports: [
         BreadcrumbComponent
